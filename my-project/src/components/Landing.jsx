b260ba2c61db320f9cb5d565c1784c75e1c85887
saumyasingh003 <saumyasingh98982@gmail.com>
2024-02-12 22:36:47 +0530

    landing

--- my-project/src/components/Landing.jsx
+++ my-project/src/components/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import image from '../assets/landing.gif'
 const Landing = () => {
   return (
-   <div className='flex md:flex-row flex-col justify-center relative lg:ml-52 max-[768px]:right-9'> 
+   <div className='flex md:flex-row flex-col justify-center relative lg:ml-80 max-[768px]:right-9'> 
    <div className=' '>
 
    <h1 className='relative items-center lg:top-32 text-4xl font-bold hover:tracking-wider hover:cursor-pointer transation-all delay-150 ease-in-out'>The Ultimate <br/>solution for <br/> your expenses..</h1>
